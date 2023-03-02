22c91b35f0c182033d7e8bd039cd59934d4830f4
Tom Gregor <tgregor@ivsoftware.com>
2023-03-02 09:09:02 -0700

    Add project files.

---
+++ chart-control-with-constant-line/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace chart_control_with_constant_line
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Location = new System.Drawing.Point(10, 25);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl1.Size = new System.Drawing.Size(478, 203);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 253);
+            this.Controls.Add(this.chartControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(10, 25, 10, 25);
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+    }
+}
+
---
+++ chart-control-with-constant-line/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.XtraCharts.Native;
+using DevExpress.XtraCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace chart_control_with_constant_line
+{
+    public partial class MainForm : DevExpress.XtraEditors.XtraForm
+    {
+        public MainForm() =>InitializeComponent();
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            // Specify data members to bind the chart's series template.
+            Series series = new Series("series", ViewType.Bar);
+            for (int i = 0; i < 25; i++)
+            {
+                series.Points.AddPoint(
+                    argument: i, 
+                    value: 1 + (0.25 - _rando.NextDouble()));
+            }
+            chartControl1.Series.Add(series);
+
+            // Specify the template's series view.
+            chartControl1.SeriesTemplate.View = new StackedBarSeriesView();
+            ((BarSeriesView)series.View).ColorEach = true;
+
+            // register chart summary function
+            // series.QualitativeSummaryOptions.SummaryFunction = "AVERAGE([WorkingDay])";
+            // ConstantLine line = new ConstantLine("Average Workingday:", series.QualitativeSummaryOptions.SummaryFunction);
+
+            ConstantLine line = new ConstantLine
+            {
+                Name= "Average Working Day",
+                AxisValue = series.Points.Average(_ => _.NumericalValue),
+                Color = Color.Red,
+            };
+
+            XYDiagram diagram = chartControl1.Diagram as XYDiagram;
+            diagram.AxisY.ConstantLines.Add(line);
+        }
+        Random _rando = new Random();
+    }
+}
